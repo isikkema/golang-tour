e240c2e8cbd0e7130eed9ad7df1013744ca60477
Isaac Sikkema <isaacsikkema@gmail.com>
2019-08-02 09:24:53 -0500

    Upload Maps.go

---
+++ Maps.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	countMap := make(map[string]int)
+	for _, word := range strings.Fields(s) {
+		countMap[word]++
+	}
+	
+	return countMap
+}
+
+func main() {
+	wc.Test(WordCount)
+}
